16a911344899a55503998b4a3a94c96d48183b97
Aishah <maishah2540@gmail.com>
2024-04-17 12:09:54 -0700

    Image Thumbnails

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import startServer from './libs/boot';
 import injectRoutes from './routes';
+import injectMiddlewares from './libs/middlewares';
 
 const server = express();
 
+injectMiddlewares(server);
 // Load routes from routes/index.js
 injectRoutes(server);
 
